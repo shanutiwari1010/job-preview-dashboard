44820ddae10fde35f6d16f3a47b4474dd6ff80b8
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-06-28 23:45:40 +0530

    feat: added files

---
+++ src/components/job-preview/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function JobPreview() {
+  return (
+    <div>
+      <h1 className='font-semibold text-2xl'>Senior Product Designer</h1>
+    </div>
+  )
+}
+
+export default JobPreview;
